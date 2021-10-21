a1edf2d8065e13fec3cdeaecca9200a066210688
M. Kastler <matthi.kastler02@gmail.com>
2021-10-21 12:28:59 +0200

    added fetch function to ChampList.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <champ-list></champ-list>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChampList from './components/ChampList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChampList
   }
 }
 </script>
---
+++ src/components/ChampList.vue
@@ -0,0 +1,65 @@
+<template>
+  <p>League of Legends Champion Table</p>
+  <table>
+    <caption>Champions</caption>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>id</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+          <td></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'ChampList',
+
+  created: function(){
+      this.fetchData()
+  },
+
+  data: function(){
+    return {
+      champions: []
+    };
+  },
+
+  methods: {
+    async fetchData(){
+      try{
+        var response = await fetch("http://ddragon.leagueoflegends.com/cdn/11.21.1/data/en_US/champion.json")
+        var json = await response.json()
+        console.log(json)
+        this.champions = json        
+      }catch(e){
+        console.log(e)
+      }
+      
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
